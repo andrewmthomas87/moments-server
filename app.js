2d549953541dd8d8783341f32c3206f7e94aae4b
Andrew Thomas <andrewmthomas87@gmail.com>
2016-07-02 23:18:57 -0600

    Testing AutoDeploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-	response.send('Hello world')
+	response.send('AutoDeploy test')
 })
 
 app.listen(8080, () => {
