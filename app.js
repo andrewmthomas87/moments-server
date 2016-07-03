b1f734c2f3dc4f88384ef5e9ccd6cec368c6108a
Andrew Thomas <andrewmthomas87@gmail.com>
2016-07-02 21:56:28 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+	response.send('Hello world')
+})
+
+app.listen(8080, () => {
+	console.log('Server listening on port 8080')
+})
